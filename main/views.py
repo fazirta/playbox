7ba36915ed883f05c03ccdf98dec3197bf8d4458
Fazil <faziltirtana@tutanota.com>
2024-09-10 20:30:30 +0700

    feat: landing page view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def landing(request):
+    context = {
+        'npm' : '2306274983',
+        'name': 'Muhammad Fazil Tirtana',
+        'class': 'PBP D',
+        'products': [
+            {
+                'name': 'Rexus Bluetooth Gamepad Gladius GX300',
+                'price': 409000,
+                'image': 'https://i.imgur.com/nVgWSZm.png'
+            }.copy() for _ in range(10)
+        ],
+    }
+
+    return render(request, "landing/index.html", context)
