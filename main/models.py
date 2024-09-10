a13ea7c44a409fd616e510e132b32120cd345a9f
Fazil <faziltirtana@tutanota.com>
2024-09-10 20:30:02 +0700

    feat: add product model to main app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.PositiveIntegerField()
+    image = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
