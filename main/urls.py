8894a6d286711ce94a2aa491fedfe5464932cecd
Fazil <faziltirtana@tutanota.com>
2024-09-16 20:19:06 +0700

    feat: create product form

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import landing
+from main.views import landing, create
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path('', landing, name='landing'),
+    path("", landing, name="landing"),
+    path("create", create, name="create"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
+
 
 def landing(request):
+    products = Product.objects.all()
+
     context = {
-        'npm' : '2306274983',
-        'name': 'Muhammad Fazil Tirtana',
-        'class': 'PBP D',
-        'products': [
-            {
-                'name': 'Rexus Bluetooth Gamepad Gladius GX300',
-                'price': 409000,
-                'image': 'https://i.imgur.com/nVgWSZm.png'
-            }.copy() for _ in range(10)
-        ],
+        "npm": "2306274983",
+        "name": "Muhammad Fazil Tirtana",
+        "class": "PBP D",
+        "products": products,
     }
 
     return render(request, "landing/index.html", context)
+
+
+def create(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect("main:landing")
+
+    context = {
+        "npm": "2306274983",
+        "name": "Muhammad Fazil Tirtana",
+        "class": "PBP D",
+        "form": form,
+    }
+
+    return render(request, "create/index.html", context)
