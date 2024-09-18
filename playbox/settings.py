2443b79b9d982af057f9fcf86aa01e09f71a06a2
Fazil <faziltirtana@tutanota.com>
2024-09-18 10:38:52 +0700

    chore: update CSRF_TRUSTED_ORIGINS

--- playbox/settings.py
+++ playbox/settings.py
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-fazil31-playbox.pbp.cs.ui.ac.id", "https://muhammad-fazil31-playbox.pbp.cs.ui.ac.id"]
