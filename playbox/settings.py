31a2489733cb72f95e67add12d06fc476fa8c732
Fazil <faziltirtana@tutanota.com>
2024-09-24 14:03:58 +0700

    fix: staticfiles not working

--- playbox/settings.py
+++ playbox/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-fazil31-playbox.pbp.cs.ui.ac.id", "https://muhammad-fazil31-playbox.pbp.cs.ui.ac.id"]
