fbb3d8b2e27a8965cc61a0c16425905cf3cb60a2
Fazil <faziltirtana@tutanota.com>
2024-12-24 18:40:16 +0700

    feat: add registration and create mood flutter routes

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
-from authentication.views import login, logout
+from authentication.views import register, login, logout
 from django.urls import path
 
 app_name = "authentication"
 
 urlpatterns = [
+    path("register/", register, name="login"),
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
 ]
